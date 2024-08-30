28ac08a0314de41e46529e0c218f346f663d1d3b
Bastet0312 <yulie.jobs0318@gmail.com>
2024-08-29 21:18:33 -0500

    Cuarta actualizacion

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template , request, redirect
+
+import db
+
 
 servidor2= Flask(__name__)
 
@@ -6,5 +9,24 @@ servidor2= Flask(__name__)
 def home():
     return render_template('index.html')
 
+@servidor2.route('/productos')
+def productos():
+    productos =db.get_all_products()
+    render_template('productos.html', productos=productos)
+    
+@servidor2.route('/productos-agregar', methods = ('GET' , 'POST'))
+
+def crear_productos():
+    if request.method =='POST':
+        nombre = request.form ['nombre']
+        descripcion= request.form ['descripcion']
+        precio= request.form ['precio']
+        imagen= request.form ['imagen']
+        
+        id.add_product (nombre , descripcion , precio , imagen)
+        return redirect(url_for('productos'))
+    return render_template('crear-productos.html', productos=None)
+
 if __name__=='__main__':
+    db.init_db()
     servidor2.run(debug=True)
